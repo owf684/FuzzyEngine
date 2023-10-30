1903185628a6c4b3cf54eff6d140167cda59b6e5
Christopher Corona <35810446+owf684@users.noreply.github.com>
2023-10-29 22:45:43 -0500

    changed main to update

--- PhysicsEngine/physics_engine.py
+++ PhysicsEngine/physics_engine.py
@@ -7,9 +7,9 @@ class PhysicsEngine:
 
 
 
-    def main(self,**kwargs):
+    def update(self,**kwargs):
         game_object = kwargs['GameObject']
         delta_t = kwargs['DeltaT']
         
         if not game_object.physics.pause:
-            game_object.current_sprite.position = game_object.physics.update(gravity=self.gravity,delta_t=delta_t)
+            game_object.physics.update(gravity=self.gravity,delta_t=delta_t)
