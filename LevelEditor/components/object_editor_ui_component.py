918b4b1a03803e69bd4de29aaf138e0af1895614
Christopher Corona <35810446+owf684@users.noreply.github.com>
2023-10-29 22:44:35 -0500

    creatd object editor ui

---
+++ LevelEditor/components/object_editor_ui_component.py
@@ -0,0 +1,55 @@
+import pygame
+import sys
+
+class ObjectEditorUIComponent:
+
+    def __init__(self):
+        self.l_object_editor_ui_elements = list()
+        display_info = pygame.display.Info()
+        self.screen_width = int(display_info.current_w*.8)
+        self.screen_height = int(display_info.current_h*.8)
+        self.object_container_path = './Assets/UI/Containers/ObjectContainer.png'
+        self.object_container_selected_path = './Assets/UI/Containers/ObjectContainer_selected.png'
+
+    def init_ui(self,c_object_creator):
+        self.l_object_editor_ui_elements.clear()
+
+        for category in c_object_creator.l_categories:
+            y_position =25
+            x_position = (self.screen_width/2) - len(c_object_creator.l_categories[c_object_creator.i_category])*64/3
+            for objects in category:
+                objects[0].physics.pause = True
+                objects[0].generic_sprite_1.create_sprite(self.object_container_path)
+                objects[0].generic_sprite_1.position.x = x_position
+                objects[0].generic_sprite_1.position.y = y_position
+
+                image_width = objects[0].current_sprite.image.get_width()
+                image_height = objects[0].current_sprite.image.get_height()
+                image = objects[0].current_sprite.image
+                x_scale_facotr = image_width/36
+                y_scale_factor = image_height/36
+                scale_factor = max(x_scale_facotr,y_scale_factor)
+                objects[0].current_sprite.image = pygame.transform.scale(image , (image_width/scale_factor, image_height/scale_factor) )
+
+                objects[0].current_sprite.position.x = x_position + objects[0].generic_sprite_1.image.get_width()/4
+                objects[0].current_sprite.position.y = y_position + objects[0].generic_sprite_1.image.get_height()/4
+
+                x_position += 64
+                self.l_object_editor_ui_elements.append(objects[0])
+                c_object_creator.update_editor_ui = True
+        
+    def update_object_container_ui(self,c_object_creator):
+        if c_object_creator.update_editor_ui:
+
+            for objects in c_object_creator.get_selected_category():
+                objects[0].generic_sprite_1.create_sprite(self.object_container_path)
+
+            c_object_creator.get_selected_object()[0].generic_sprite_1.create_sprite(self.object_container_selected_path)
+
+            self.l_object_editor_ui_elements.clear()
+
+            for objects in c_object_creator.get_selected_category():
+                self.l_object_editor_ui_elements.append(objects[0])
+
+            c_object_creator.update_editor_ui = False
+
