f1a28f1ae060976dddc3dce2c8169f966e91231c
Christopher Corona <christophecorona1996@gmail.com>
2023-10-31 14:02:45 -0500

    added button handling to class

--- LevelEditor/components/button_ui_component.py
+++ LevelEditor/components/button_ui_component.py
@@ -14,9 +14,10 @@ class ButtonUIComponent:
 
         self.sprite = sprite_component.SpriteComponent()
         self.button_state = ButtonState(0,1)
+        self.button_signal = ButtonSignal()
         self.toggled = False
 
-    def update(self,d_inputs):
+    def set_animation_state(self,d_inputs):
 
         mouse_position = pygame.mouse.get_pos()
         # update animation if mouse hovers over button
@@ -32,11 +33,54 @@ class ButtonUIComponent:
         if not d_inputs['left-click'] and d_inputs['left_click_latch']:
             d_inputs['left_click_latch'] = False
 
+    def draw_button(self,screen):
+        if len(self.sprite.sprite_sheet)>0:
+                if self.sprite.sprite_sheet[self.sprite.animation_state] is not None:
+                    screen.blit(self.sprite.sprite_sheet[self.sprite.animation_state],(self.sprite.position.x,self.sprite.position.y))
+
+    def update(self,**kwargs):
+        d_inputs = kwargs['InputDict']
+        key = kwargs['Key']        
+        self.set_animation_state(d_inputs)
+        match key:
+            case 'play':
+                if self.toggled:
+                    if (self.button_state.state_1 == 0): self.button_state = ButtonState(2,3)
+                    elif (self.button_state.state_1 == 2): self.button_state = ButtonState(0,1)
+                    self.toggled = False
+                    self.button_signal.send(**kwargs)
+            case 'add':
+                if self.toggled:
+                    self.toggled = False   
+            case 'back':
+                if self.toggled:
+                    self.toggled = False 
+                    self.button_signal.send(**kwargs)
+
+            case _:
+                None
+            
 class ButtonState:
     
     def __init__(self,state_1,state_2):
         self.state_1 = state_1
-        self.state_2 = state_2  
+        self.state_2 = state_2 
+
+
+class ButtonSignal:
+
+    def send(self,**kwargs):
+        key = kwargs['Key']
+        level_editor = kwargs['ALevelEditor']
+       
+        match key:
+            case 'play':
+                level_editor.edit = not level_editor.edit
+
+            case 'back':
+                level_editor.attribute_ui.restore_attribute_components()
+            case _:
+                None
 
        
 
