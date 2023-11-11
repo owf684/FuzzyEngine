a642c652ee8195d19a61fc975f0f48cf0770e3ce
Christopher Corona <35810446+owf684@users.noreply.github.com>
2023-11-10 23:42:02 -0600

    removed player logic from here

--- PlayerEngine/player_engine.py
+++ PlayerEngine/player_engine.py
@@ -4,20 +4,9 @@ import pygame
 class PlayerEngine:
 
     def __init__(self):
-        self.player_walking_force = 20000
-        self.thrust_force = -100000
+        None
     def update(self,**kwargs):
         player_object = kwargs['PlayerObject']
-        input_dict = kwargs['InputDict']
-        self.x_movement(player_object,input_dict)
-        #self.y_movement(player_object,input_dict)
-
-
-
-    def x_movement(self,player_object,input_dict):
-        player_object.physics.direction.x = input_dict['horizontal']
-        player_object.physics.force.x = self.player_walking_force*player_object.physics.direction.x
-
-    def y_movement(self,player_object,input_dict):
-        player_object.physics.direction.y = input_dict['vertical']
-        player_object.physics.initial_velocity.y = self.thrust_force*player_object.physics.direction.y
+        delta_t=kwargs['DeltaT']
+        player_object.update(InputDict=kwargs['InputDict'],DeltaT=delta_t)
+    
