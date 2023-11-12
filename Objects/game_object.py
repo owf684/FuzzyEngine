e4167af0d4574d3526d31429bcb6f2bac064734a
Christopher Corona <35810446+owf684@users.noreply.github.com>
2023-11-12 00:01:06 -0600

    added save component to game object

--- Objects/game_object.py
+++ Objects/game_object.py
@@ -3,17 +3,22 @@ sys.path.append("./Objects/components")
 import sprite_component
 import physics_component
 import collider_component
-
+import save_component
 class GameObject:
 
     def __init__(self):
+        # sprites
         self.current_sprite   = sprite_component.SpriteComponent()
         self.generic_sprite_1 = sprite_component.SpriteComponent()
         self.generic_sprite_2 = sprite_component.SpriteComponent()
         self.generic_sprite_3 = sprite_component.SpriteComponent()
         self.generic_sprite_4 = sprite_component.SpriteComponent()
 
+        # physics
         self.physics = physics_component.PhysicsComponent()
         
-        # passing object to collider component for future use
+        # collider
         self.collider = collider_component.ColliderComponent()
+
+        # save state
+        self.save_state = save_component.SaveComponent()
