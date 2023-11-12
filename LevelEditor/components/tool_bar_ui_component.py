d6a62581eff9987ff50f3dc7a9f4ff76bf76b039
Christopher Corona <35810446+owf684@users.noreply.github.com>
2023-11-12 00:00:21 -0600

    add scene tool bar background to the bottom

--- LevelEditor/components/tool_bar_ui_component.py
+++ LevelEditor/components/tool_bar_ui_component.py
@@ -14,9 +14,11 @@ class ToolBarUIComponent:
         self.tool_bar_size = Vector(400,self.display_height + 200) 
         self.tool_bar_position = Vector(self.display_width,0)
 
-    
+        self.tool_bar_2_position = Vector(0,self.display_info.current_h*.8-15)
+        self.tool_bar_2_size = Vector(self.display_width,self.display_info.current_h*.2+15)
     def draw_toolbar(self,screen):
 
         pygame.draw.rect(screen,self.tool_bar_color, (self.tool_bar_position.x,self.tool_bar_position.y,self.tool_bar_size.x,self.tool_bar_size.y))
     
-        
+        pygame.draw.rect(screen,self.tool_bar_color, (self.tool_bar_2_position.x,self.tool_bar_2_position.y,self.tool_bar_2_size.x,self.tool_bar_2_size.y))
+
