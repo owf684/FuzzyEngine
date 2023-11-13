1cac96b471b4bb2634cc6fbcdc5da4f3d11cbb8c
Christopher Corona <35810446+owf684@users.noreply.github.com>
2023-11-12 23:09:36 -0600

    updated player_engine with instance check checking for player object before updating

--- PlayerEngine/player_engine.py
+++ PlayerEngine/player_engine.py
@@ -1,12 +1,15 @@
 import pygame
-
+import sys
+sys.path.append('./GameData/GameObjects')
+import player_object
 
 class PlayerEngine:
 
     def __init__(self):
         None
     def update(self,**kwargs):
-        player_object = kwargs['PlayerObject']
+        o_player = kwargs['PlayerObject']
         delta_t=kwargs['DeltaT']
-        player_object.update(InputDict=kwargs['InputDict'],DeltaT=delta_t)
+        if isinstance(o_player,player_object.PlayerObject):
+            o_player.update(InputDict=kwargs['InputDict'],DeltaT=delta_t)
     
