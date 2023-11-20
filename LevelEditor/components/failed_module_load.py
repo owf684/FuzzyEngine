89f32aec46d5da70f5b325f7e0c71eeffcc90fbf
Christopher Corona <christophecorona1996@gmail.com>
2023-11-20 11:59:45 -0600

    corrected file name for png and json

--- LevelEditor/components/failed_module_load.py
+++ LevelEditor/components/failed_module_load.py
@@ -2,11 +2,11 @@ import sys
 sys.path.append('./Objects')
 sys.path.append('./Objects/components')
 import game_object
-class WarpPipe(game_object.GameObject):
+class FailedModuleLoad(game_object.GameObject):
 		def __init__(self):
 			super().__init__()
-			self.current_sprite.create_sprite('./LevelEditor/components/FailedModuleLoad/donuts.png')
+			self.current_sprite.create_sprite('./LevelEditor/components/FailedModuleLoad/donut.png')
 			self.physics.pause = True
-			self.save_state.object_json_file='./LevelEditor/components/FailedModuleLoad/donuts.json'
+			self.save_state.object_json_file='./LevelEditor/components/FailedModuleLoad/donut.json'
 def create_object():
-	return WarpPipe()
+	return FailedModuleLoad()
