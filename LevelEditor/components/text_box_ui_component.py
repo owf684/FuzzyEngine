4d6bd63c7690122f6dd4b4d5d32e4fbf04fc6868
Christopher Corona <35810446+owf684@users.noreply.github.com>
2023-11-19 21:57:20 -0600

    adjusted color, position of text, and look of text box

--- LevelEditor/components/text_box_ui_component.py
+++ LevelEditor/components/text_box_ui_component.py
@@ -4,8 +4,6 @@ import pygame.freetype as freetype
 sys.path.append("./Objects/components")
 from vector import Vector
 
-global BGCOLOR, PRINT_EVENT, CHATBOX_POS, CHATLIST_POS, CHATLIST_MAXSIZE
-
 
 ChatList = []
 class TextBoxUIComponent:
@@ -19,17 +17,20 @@ class TextBoxUIComponent:
         self.input_text = ''
         self.event = None
 
-    def get_input(self):
+    def get_input(self,**kwargs):
+        d_input = kwargs['InputDict']
         mouse_position = pygame.mouse.get_pos()
         for text_box in self.l_text_boxes:
             if text_box.rect.collidepoint(mouse_position):
-                if pygame.MOUSEBUTTONDOWN:
+                if d_input['left-click']:
                     if text_box != self.selected_text_box:
                         if self.selected_text_box is not None:
                             self.selected_text_box.selected = False
+                            self.selected_text_box.text_box_color = text_box.unselected_color
                         text_box.selected = True
+                        text_box.text_box_color =text_box.selected_color
                         self.selected_text_box = text_box
-                        self.input_text = self.selected_text_box.userInput
+                        self.input_text = self.selected_text_box.userInput 
         self.handle_input_events()
 
     def handle_input_events(self):
@@ -88,12 +89,21 @@ class TextBox:
         "msmincho",
         "Arial",
         ]
+        self.selected_color = (100,100,100)
+        self.unselected_color = (150,150,150)
+        self.text_box_color = (150,150,150)
         self.font_small = freetype.SysFont(self.font_names, 12)
 
         pygame.key.start_text_input()
         pygame.key.set_text_input_rect(self.rect)
     def draw_text_box(self,screen):
-        pygame.draw.rect(screen,(255,255,255),self.rect)
-        self.font_small.render_to(screen, self.rect, self.userInput, (0,0,0))
+        pygame.draw.rect(screen,self.text_box_color,self.rect)
+        pygame.draw.rect(screen,(50,50,50),self.rect,2)
+        text_rect= pygame.Rect(self.rect.x+5,self.rect.y+self.rect.height/2,self.rect.width,self.rect.height)
+        if len(self.userInput)*7 > self.rect.width:
+            userInput = self.userInput[:self.rect.width//7]
+        else:
+            userInput = self.userInput
+        self.font_small.render_to(screen, text_rect, userInput, (255,255,255))
 
 
