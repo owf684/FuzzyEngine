1f52e76ce65a4c952c7613e264ee0836c015013e
Christopher Corona <35810446+owf684@users.noreply.github.com>
2023-11-27 23:00:06 -0600

    added a flag for pausing physics x position

--- Objects/components/physics_component.py
+++ Objects/components/physics_component.py
@@ -6,6 +6,7 @@ class PhysicsComponent:
   def __init__(self):
     self.position = Vector(0,0)
     self.initial_position = Vector(0,0)
+    self.displacement = Vector(0,0)
     self.velocity = Vector(0,0)
     self.initial_velocity = Vector(0,0)
     self.acceleration = Vector(0,0)
@@ -20,4 +21,4 @@ class PhysicsComponent:
     self.pause = False
     self.gravity = 0
     self.gravity_force = 0
-
+    self.pause_x_position = False
