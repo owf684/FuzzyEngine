eb1cdbca301b76e0b8ffeaf46a7ea97ae7b90704
Christopher Corona <35810446+owf684@users.noreply.github.com>
2023-11-30 23:05:21 -0600

    expanded scroll engine functionality by allowing grid movement in edit mode

--- ScrollEngine/scroll_engine.py
+++ ScrollEngine/scroll_engine.py
@@ -1,8 +1,9 @@
 import sys
 import pygame
+
 sys.path.append('./GameData/GameObjects')
 import player_object
-
+from copy import deepcopy
 
 
 class ScrollEngine:
@@ -10,46 +11,56 @@ class ScrollEngine:
     def __init__(self):
         display_info = pygame.display.Info()
         self.screen_width = int(display_info.current_w)
-        self.screen_height = int(display_info.current_h*.87)
+        self.screen_height = int(display_info.current_h * .87)
         self.displacement_x = 0
         self.scroll_offset = 0
-        self.scroll_threshold_right = self.screen_width*.4
-        self.scroll_threshold_left = self.screen_width*.2
+        self.scroll_threshold_right = self.screen_width * .4
+        self.scroll_threshold_left = self.screen_width * .2
 
         self.scrolling = False
 
     def update(self, **kwargs):
         objects = kwargs['GameObject']
 
-        if isinstance(objects,player_object.PlayerObject):
+        if isinstance(objects, player_object.PlayerObject):
             if objects.physics.position.x > self.scroll_threshold_right and objects.physics.direction.x > 0 or \
-                objects.physics.position.x < self.scroll_threshold_left and objects.physics.direction.x < 0:
+                    objects.physics.position.x < self.scroll_threshold_left and objects.physics.direction.x < 0:
 
                 self.scrolling = True
-            else:
+            elif objects.physics.direction.x == 0 and int(abs(objects.physics.velocity.x)) < 0.5:
                 self.scrolling = False
-          
+
             if self.scrolling:
                 self.displacement_x = objects.physics.displacement.x
                 self.scroll_offset += self.displacement_x
-                  
+
                 objects.physics.pause_x_position = True
             else:
                 objects.physics.pause_x_position = False
 
-
-
-    def scroll_objects(self,**kwargs):
+    def scroll_objects(self, **kwargs):
 
         game_objects = kwargs['GameObjects']
         level_editor = kwargs['LevelEditor']
-
-        if self.scrolling:
+        input_dict = kwargs['InputDict']
+        if self.scrolling and not level_editor.edit:
 
             for objects in game_objects:
-                if not isinstance(objects,player_object.PlayerObject):
+                if not isinstance(objects, player_object.PlayerObject):
                     objects.physics.position.x -= self.displacement_x
 
-        level_editor.grid.scroll_offset = self.scroll_offset
-              
-    
+        if level_editor.edit and level_editor.scroll_in_edit:
+            if abs(input_dict['horizontal']) > 0:
+                if input_dict['l-shift'] == 1:
+                    self.displacement_x = 5*input_dict['horizontal']
+                else:
+                    self.displacement_x = 1*input_dict['horizontal']
+                self.scroll_offset += self.displacement_x
+                for objects in game_objects:
+                    objects.physics.position.x -= self.displacement_x
+                    objects.current_sprite.update(objects.physics.position)
+        if level_editor.grid.reset_scroll:
+            self.scroll_offset = 0
+            level_editor.grid.reset_scroll = False
+        else:
+            level_editor.grid.scroll_offset = deepcopy(self.scroll_offset)
