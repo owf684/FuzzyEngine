c38a7265e9d49aa432ac421a2c90e79e0fa50be0
Christopher Corona <35810446+owf684@users.noreply.github.com>
2023-12-04 22:14:03 -0600

    added booleans to turn off colliders in all or specific directions

--- Objects/components/collider_component.py
+++ Objects/components/collider_component.py
@@ -7,7 +7,18 @@ class ColliderComponent:
         self.right = False
         self.down = False
         self.left = False
-    
+
+        self.left_collision_object = None
+        self.right_collision_object = None
+        self.up_collision_object = None
+        self.down_collision_object = None
+
+        self.all_off = False
+        self.pass_through = False
+        self.left_off = False
+        self.right_off = False
+        self.up_off = False
+        self.down_off = False
 
     def reset(self):
         self.up = False
